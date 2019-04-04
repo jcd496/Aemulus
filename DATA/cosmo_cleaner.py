6bc6239ae76eed18d17f86eabb97234a75fafc06
John <jcd496@nyu.edu>
2019-04-04 11:11:13 -0400

    added scaling

--- DATA/cosmo_cleaner.py
+++ DATA/cosmo_cleaner.py
@@ -1,13 +1,14 @@
-import numpy as np 
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 hod_params = 8
 cosmo_params = 7
 rbin= 9
 
-#radius + wp + error= + 3 params for total of 18 columns
+#radius + wp + error + bin_num= +4 params for total of 19 columns
 #40 cosmo x 2000 hod x 9 radius buckets = 720,000 rows of data
 hod_data = np.genfromtxt("HOD_parameters_5000.dat", delimiter=" ", dtype=float)
 cosmo_data = np.genfromtxt("cosmological_parameters_full.dat", delimiter="", dtype=float)
-all_data_training=np.empty((720000,18))
+all_data_training=np.empty((720000,19))
 
 for cosmo in range(34): #40
 
@@ -25,11 +26,16 @@ for cosmo in range(34): #40
 			full_data = np.append(full_data, radius)
 			full_data = np.append(full_data, wp)
 			full_data = np.append(full_data, error)	
+            full_data = np.append(full_data, rad)  #bin number
 			
-				#complete_training_data[2000*9*cosmo+9*hod+rad]=full_data
 			all_data_training[cosmo*2000*rbin + hod*rbin+rad]=full_data
 
 np.random.shuffle(all_data_training)
+
+x = all_data_training[:,:16]
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+all_data_training = np.append(x_scaled, all_data_training[:,16:], 1)
 np.savetxt('cosmo_training_data.csv', all_data_training[:540000,:], fmt='%f', delimiter=',')
 np.savetxt('cosmo_test_data.csv',all_data_training[540000:,:],fmt='%f',delimiter=',')
 
